d1c00d823c7e5b2ea28cb722b4a6e5bb2814aaf8
Andrei Cociorvan <84695298+andrei99c@users.noreply.github.com>
2023-02-15 15:30:23 +0200

    Add files via upload

---
+++ serial-rx-tx/serial-rx-tx.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+
+#define rxPin 3
+#define txPin 4
+
+SoftwareSerial softSerial =  SoftwareSerial(rxPin, txPin);
+
+void setup()  {
+ 
+ pinMode(rxPin, INPUT);
+ pinMode(txPin, OUTPUT);
+ softSerial.begin(9600);
+
+ Serial.begin(115200);
+}
+
+void loop() {
+ 
+
+ softSerial.println("AT+GMR");
+ delay(1000);
+ Serial.println(softSerial.readStringUntil('\n'));
+ 
+
+}
